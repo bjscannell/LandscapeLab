7e512d1473eb4aa4fd8043c54875073a9af1e24c
Brittney Scannell <brittney.scannell@stonybrook.edu>
2024-07-24 13:22:42 -0400

    combine detections and ignoring output/tags

--- Read_In.R
+++ Read_In.R
@@ -1,1 +1,15 @@
 ## Read in Data
+
+source("https://raw.githubusercontent.com/bjscannell/lab_code/master/load_vps_csvs.R")
+
+tags <- read_csv("Acoustic_tags_PetersonLab.csv", 
+                 col_types = cols(UTC_RELEASE_DATE_TIME = col_datetime(format = "%Y-%m-%d %H:%M:%S"))) %>% 
+  mutate(FullId = paste(TAG_CODE_SPACE, "-", TAG_ID_CODE, sep = ""))
+
+raw_dets <- load_vps_files()
+
+dets <- raw_dets %>% filter(HPEs <= 10) %>%
+  left_join(tags, by = "FullId")
+
+
+write_csv(dets, "filtered_detections.csv")
